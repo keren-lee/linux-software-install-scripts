9dc23a9ac1c875e82b21591a99ebefd14b1e965d
lyot <lyot520@gmail.com>
2018-11-06 21:44:26 +0800

    update

--- docker/docker-ubuntu16.sh
+++ docker/docker-ubuntu16.sh
@@ -1,20 +1,21 @@
-#!/bin/bash
-sudo apt-get remove docker docker-engine docker.io
-sudo apt-get update
-sudo apt-get install \
+#! /bin/bash
+sudo apt remove docker docker-engine docker.io
+sudo apt update
+sudo apt install \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-echo "start install docker"
-sudo apt-get install -y docker-ce
+sudo apt update
+echo "start install docker..."
+sudo apt install -y docker-ce
 echo "start install docker compose"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo docker-compose --version
+# sudo chmod -aG docker $USER
 
 
 # The fllow is offical instroduction document
