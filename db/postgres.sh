c84a40a680d0de11fd6854a49462a186f7f3d011
tyunglee <tyung-lee@foxmail.com>
2018-11-07 10:21:03 +0800

    update

---
+++ db/postgres.sh
@@ -0,0 +1,5 @@
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main"
+sudo apt update
+# choose the vision what you want to install
+sudo apt install -y postgresql-11
