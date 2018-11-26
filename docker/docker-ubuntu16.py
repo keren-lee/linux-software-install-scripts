fab0565c1e1a0f03cebe7d59e56637a550b39418
tyunglee <tyung-lee@foxmail.com>
2018-11-26 16:33:56 +0800

    update

--- docker/docker-ubuntu16.py
+++ docker/docker-ubuntu16.py
@@ -6,8 +6,8 @@ def excute(commands):
         status = subprocess.call(command,shell=True)
 
 commands = [
-    'sudo apt remove docker docker-engine docker.io',
     'sudo apt update',
+    'sudo apt remove docker docker-engine docker.io',
     'sudo apt install -y apt-transport-https ca-certificates curl software-properties-common',
     'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -',
     'sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"',
